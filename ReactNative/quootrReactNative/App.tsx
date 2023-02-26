2e07e24a4a90c29befb3abfa33f7e5f51a1ca543
Buuhzerah <59845679+Buuhzerah@users.noreply.github.com>
2023-02-25 22:55:13 -0300

    Tela de registro
    Criada a tela de registro e o arquivo index como a base de rotas

--- ReactNative/quootrReactNative/App.tsx
+++ ReactNative/quootrReactNative/App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from "expo-status-bar";
+/*import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LoginScreen from "./src/screens/LoginScreen/login";
 
 import colors from "./src/colors";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import RegisterScreen from "./src/screens/register";
 
 export default function App() {
   const [loaded] = useFonts({
@@ -43,3 +44,37 @@ const styles = StyleSheet.create({
     color: colors.quootrWhite,
   },
 });
+
+*/
+
+import React from 'react';
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import { StatusBar } from 'expo-status-bar';
+
+import { NavigationContainer } from '@react-navigation/native'
+import Routes from './src/routes';
+
+export default function App() {
+  const [loaded] = useFonts({
+    "SpaceGrotesk-Regular": require("./assets/fonts/SpaceGrotesk-Regular.ttf"),
+    "SpaceGrotesk-Medium": require("./assets/fonts/SpaceGrotesk-Medium.ttf"),
+    "SpaceGrotesk-Bold": require("./assets/fonts/SpaceGrotesk-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (!loaded) {
+      return;
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return
+  }
+
+  return (
+    <NavigationContainer>
+      <Routes/>
+    </NavigationContainer>
+  )
+}
